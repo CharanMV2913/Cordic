778443db11a9790b2e85cc10a559a286aaf222b2
CharanMVFlexAi <charan.mv@flex.ai>
2024-05-14 19:30:38 +0530

    Complete files uploded

--- Cordic/fv/Cordic/read_libs.tcl
+++
@@ -1,3 +0,0 @@
-add_search_path /home/internals/Desktop/gpdk045_libfiles_1 -library -both
-read_library -liberty -both /home/internals/Desktop/gpdk045_libfiles_1/fast.lib /home/internals/Desktop/gpdk045_libfiles_1/typical.lib /home/internals/Desktop/gpdk045_libfiles_1/slow.lib
-
--- Cordic/temp/genus.cmd
+++
@@ -1,8 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 20.11-s111_1, built Apr 26 2021 14:57:38
-
-# Date: Fri Sep 01 17:55:04 2023
-# Host: sys128 (x86_64 w/Linux 3.10.0-1062.el7.x86_64) (8cores*16cpus*1physical cpu*11th Gen Intel(R) Core(TM) i7-11700 @ 2.50GHz 16384KB)
-# OS:   CentOS Linux release 7.7.1908 (Core)
-
-source ~/Desktop/Cordic/temp.tcl
-exit
